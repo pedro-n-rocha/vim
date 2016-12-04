1e63551505a2d283aced63110518f186308bc9f0
toor <toor@apollo.workgroup>
2016-12-04 11:58:12 -0500

    add ycm

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,4 +11,9 @@ git clone https://github.com/scrooloose/syntastic ~/.vim/bundle/syntastic
 git clone https://github.com/majutsushi/tagbar ~/.vim/bundle/tagbar 
 git clone https://github.com/tpope/vim-fugitive ~/.vim/bundle/vim-fugitive 
 git clone https://github.com/airblade/vim-gitgutter ~/.vim/bundle/vim-gitgutter 
+git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
 ln -s $(pwd)/.vimrc ~/.vimrc
+
+echo "\n\n **** YouCompleteMe instructions **** \n\n"
+echo "cd ~/.vim/bundle/YouCompleteMe && git submodule update --init --recursive  && python install.py --clang-completer " 
+
